fc174e4e431a7f13184dd181d9cc5f2b902ebeb9
dongjiujin <dongjiujin_wp.com>
2025-04-21 21:17:35 +0800

    UAV Trajectory and UGV Fixed

---
+++ UWB_Simulation/LM.py
@@ -0,0 +1,118 @@
+import math
+import random
+import matplotlib.pyplot as plt
+from mpl_toolkits.mplot3d import Axes3D
+
+class LM3DPositioning:
+    def __init__(self, anchors, true_pos = None):
+        self.anchors = anchors
+        self.true_pos = true_pos
+
+    def visualize_3d(self, estimated_pos):
+        fig = plt.figure()
+        ax = fig.add_subplot(111, projection = '3d')
+
+        xs, ys, zs = zip(*self.anchors)
+
+        ax.scatter(xs, ys, zs, c = 'blue', label = 'Anchors', s = 50)
+        ax.scatter([self.true_pos[0]], [self.true_pos[1]], [self.true_pos[2]], c = 'red', label = 'True Position', s = 100, marker = 'x')
+        ax.scatter([estimated_pos[0]], [estimated_pos[1]], [estimated_pos[2]], c = 'green', label = 'Estimated Position', s = 100, marker = '^')
+        ax.plot([self.true_pos[0], estimated_pos[0]], [self.true_pos[1], estimated_pos[1]], [self.true_pos[2], estimated_pos[2]], 'r--', label = 'Error Line')
+
+        ax.set_title('3D Positioning USE LM')
+        ax.set_xlabel('X')
+        ax.set_ylabel('Y')
+        ax.set_zlabel('Z')
+
+        ax.legend()
+        ax.grid(True)
+        plt.tight_layout()
+        plt.show()
+
+    # 建模 计算测的距离差 和模型的距离差
+    def compute_residuals(self, distance, params):
+        x, y, z = params
+        residuals = []
+        for (xi, yi, zi), di in zip(self.anchors, distance): 
+            Ri = math.sqrt((x - xi)**2 + (y - yi)**2 + (z - zi)**2)
+            residuals.append(Ri - di)
+        return residuals
+    # 计算雅可比矩阵
+    def compute_jacobian(self, params):
+        x, y, z = params
+        j = []
+        for (xi, yi, zi) in self.anchors:
+            dx = x - xi
+            dy = y - yi
+            dz = z - zi
+            Ri = math.sqrt(dx**2 + dy**2 + dz**2)
+            if Ri == 0:
+                Ri = 1e-8
+            j.append([dx/Ri, dy/Ri, dz/Ri])
+        return j
+    # 矩阵转置
+    def transpose(self, mat):
+        return [[mat[j][i] for j in range(len(mat))] for i in range(len(mat[0]))]
+    # 矩阵相乘
+    def mat_mul(self, A, B):
+        result = [[0.0 for _ in range(len(B[0]))] for _ in range(len(A))] # 初始化结果矩阵
+        for i in range(len(A)):
+            for j in range(len(B[0])):
+                for k in range(len(B)):
+                    result[i][j] += A[i][k] * B[k][j]
+        return result
+    # 解线性方程组 Ax = b 高斯消元法
+    def  solve_linear(self, A, b):
+        n = len(A)
+        for i in range(n):
+            # 主元选取
+            max_row = max(range(i, n), key = lambda r : abs(A[r][i]))
+            A[i], A[max_row] = A[max_row], A[i]
+            b[i], b[max_row] = b[max_row], b[i]
+            # 消元操作
+            for j in range(i + 1, n):
+                factor = A[j][i] / A[i][i]
+                for k in range(i, n):
+                    A[j][k] -= factor * A[i][k]
+                b[j] -= factor * b[i]
+            # 回带求解
+            x = [0.0] * n
+            for i in reversed(range(n)):
+                x[i] = (b[i] - sum(A[i][j] * x[j] for j in range(i + 1, n))) / A[i][i]
+        return x
+    # LM算法
+    def levenberg_marquardt(self, distance, init_params, max_iter = 100, lambda_init = 0.01, verbose = False):
+        params = init_params[:]
+        lambd = lambda_init
+        for iteration in range(max_iter):
+            residuals = self.compute_residuals(distance, params)
+            J = self.compute_jacobian(params)
+
+            JT = self.transpose(J)
+            JTJ = self.mat_mul(JT, J)
+
+            for i in range(len(JTJ)):
+                JTJ[i][i] += lambd
+
+            JTr = self.mat_mul(JT, [[r] for r in residuals])
+            JTr = [row[0] for row in JTr]
+
+            delta = self.solve_linear([row[:] for row in JTJ], [-v for v in JTr])
+            new_params = [p + d for p, d in zip(params, delta)]
+            new_residuals = self.compute_residuals(distance, new_params)
+
+            old_error = sum(r**2 for r in residuals)
+            new_error = sum(r**2 for r in new_residuals)
+
+            if new_error < old_error: # 误差减小接受新参数
+                params = new_params
+                lambd *= 0.7  
+            else: # 误差增大拒绝更新
+                lambd *= 2.0
+            
+            if max(abs(d) for d in delta) < 1e-6:
+                break
+            if verbose:
+                print(f"Iter {iteration + 1} : params = {params}, error = {new_error: .6f}")
+        return params
+
--- UWB_Simulation/uwb_simulation.py
+++ UWB_Simulation/uwb_simulation.py
@@ -1,56 +1,17 @@
 # 验证UWB测距方法
-import sys
-import os
 import math
 import numpy as np
-import matplotlib.pyplot as plot
+import matplotlib.pyplot as plt
+from LM import LM3DPositioning
 
 class Uwb_Simulation:
     def __init__(self):
         # ugv质心的位置坐标
-        self.x0 = 0
-        self.y0 = 0
-        self.z0 = 0
-        self.loaction_ugv = np.zeros((self.x0, self.y0, self.z0))
-        # 小车上3个uwb节点位置坐标
-        self.x1 = 0
-        self.y1 = 0
-        self.z1 = 0
-        self.location_uwb1 = np.zeros((self.x1, self.y1, self.z1))
-        self.x2 = 0
-        self.y2 = 0
-        self.z2 = 0
-        self.loaction_uwb2 = np.zeros((self.x2, self.y2, self.z2))
-        self.x3 = 0
-        self.y3 = 0
-        self.z3 = 0
-        self.loaction_uwb3 = np.zeros((self.x3, self.y3, self.z3))
-        # drone的位置坐标
-        self.x_t = 0
-        self.y_t = 0
-        self.z_t = 0
-        self.location_drone = np.zeros((self.x_t, self.y_t, self.z_t))
-        # drone 到三个uwb节点的距离
-        self.d1 = 0
-        self.d2 = 0
-        self.d3 = 0
-    
-    def Fomulation_Solve(self, a1, b1, c1, a2, b2, c2):
-        denominator = a1*b2 -a2*b1
-        if denominator == 0:
-            return "no only solution"
-        x = (c1*b1 - c2*b1)/denominator
-        y = (a1*c2 - a2*c1)/denominator
-        return x,y
-
-
-    def Distance_Measured(self):
-        # ugv 质心
         self.x0 = 0.2
         self.y0 = 0
         self.z0 = 0.1
         self.location_ugv = np.array([self.x0, self.y0, self.z0])
-        # 三个uwb锚点的位置坐标
+        # 小车上3个uwb节点位置坐标
         self.x1 = 0
         self.y1 = 0.2
         self.z1 = 0.1
@@ -58,27 +19,88 @@ class Uwb_Simulation:
         self.x2 = 0
         self.y2 = -0.2
         self.z2 = 0.1
-        self.loaction_uwb2 = np.array([self.x2, self.y2, self.z2])
+        self.location_uwb2 = np.array([self.x2, self.y2, self.z2])
         self.x3 = 0.4
         self.y3 = -0.2
         self.z3 = 0.1
         self.location_uwb3 = np.array([self.x3, self.y3, self.z3])
-        # ugv 地系坐标
-        self.x_t = 5
-        self.y_t = 5
-        self.z_t = 5.1
+        # drone的位置坐标
+        self.x_t = 0
+        self.y_t = 0
+        self.z_t = 0
+        self.location_drone = np.array([self.x_t, self.y_t, self.z_t])
+        # drone 到三个uwb节点的距离
+        self.d1 = 0
+        self.d2 = 0
+        self.d3 = 0
+        #记录数据
+        self.time_step = []
+        self.true_position = []
+        self.caculated_position = []
+
+    # 解析解解法
+    # def Fomulation_Solve(self, a1, b1, c1, a2, b2, c2):
+    #     denominator = a1*b2 -a2*b1
+    #     if denominator == 0:
+    #         return "no only solution"
+    #     x = (c1*b2 - c2*b1)/denominator
+    #     y = (a1*c2 - a2*c1)/denominator
+    #     return x,y
+
+
+    def Distance_Measured(self, t):
+        # uav 地系坐标
+        self.x_t = 5 + 0.1 * t
+        self.y_t = 5 + 0.2 * t
+        self.z_t = 5.1 + 0.05 * t
         # 计算uav到各个锚点的距离
         self.d1 = math.sqrt((self.x_t - self.x1)**2 + (self.y_t - self.y1)**2 + (self.z_t -self.z1)**2)
         self.d2 = math.sqrt((self.x_t - self.x2)**2 + (self.y_t - self.y2)**2 + (self.z_t -self.z2)**2)
         self.d3 = math.sqrt((self.x_t - self.x3)**2 + (self.y_t - self.y3)**2 + (self.z_t -self.z3)**2)
-        print(self.d1, self.d2, self.d3)
         #分别以三个锚点的坐标为圆心，到uav的距离为半径得到三个球并求交点获得uav计算出来的坐标
-        self.x_c, self.y_c = self.Fomulation_Solve(2*(self.x2 - self.x1), 2*(self.y2-self.y1), self.d1**2 - self.d2**2 +(self.x2 - self.x1)**2 + (self.y2 - self.y1)**2, 2*(self.x3 - self.x1), 2*(self.y3 - self.y1), self.d1**2 - self.d3**2 + (self.x3 - self.x1)**2 + (self.y3 - self.y1)**2)
-        self.z_c = math.sqrt(self.d1**2 - (self.x_c - self.x1)**2 - (self.y_c - self.y1)**2) + self.z1
-        print(self.x_c, self.y_c, self.z_c)
+        # print(self.d1, self.d2, self.d3)
+        # self.x_c, self.y_c = self.Fomulation_Solve(2*(self.x2 - self.x1), 2*(self.y2-self.y1), self.d1**2 - self.d2**2 +(self.x2 - self.x1)**2 + (self.y2 - self.y1)**2, 2*(self.x3 - self.x1), 2*(self.y3 - self.y1), self.d1**2 - self.d3**2 + (self.x3 - self.x1)**2 + (self.y3 - self.y1)**2)
+        # self.z_c = math.sqrt(self.d1**2 - (self.x_c - self.x1)**2 - (self.y_c - self.y1)**2) + self.z1
+        anchors = [
+            self.location_uwb1,
+            self.location_uwb2,
+            self.location_uwb3
+        ]
+
+        true_pos = [self.x_t, self.y_t, self.z_t]
+        distances = [self.d1, self.d2, self.d3]
+        lm = LM3DPositioning(anchors, true_pos)
+        initial_guess = [0.0, 0.0, 1.0]
+        estimated = lm.levenberg_marquardt(distances, initial_guess, verbose = True)
+        print("Estimated Position:", estimated)
+        if t == 99:
+            lm.visualize_3d(estimated)
+
+        self.time_step.append(t)
+        self.true_position.append((self.x_t, self.y_t, self.z_t))
+        self.caculated_position.append(tuple(estimated))
+
+    def plot_results(self):
+        true_position = np.array(self.true_position)
+        caculated_position = np.array(self.caculated_position)
+        
+        fig, axes = plt.subplots(3, 1, figsize = (10, 8))
+        labels = ['X Position', 'Y Position', 'Z Position']
+        for i in range(3):
+            axes[i].plot(self.time_step, true_position[:,i], label = 'True Postion', linestyle = '-',)
+            axes[i].plot(self.time_step, caculated_position[:,i], label = 'Caculated Position', linestyle = '--')
+            axes[i].set_ylabel(labels[i])
+            axes[i].legend()
+            axes[i].grid()
+        
+        axes[2].set_xlabel('time step')
+        plt.suptitle('True and Estimated UAV position')
+        plt.show()
 
 if __name__ == "__main__":
     uwb = Uwb_Simulation()
-    uwb.Distance_Measured()
+    for t in range(0,100):
+        uwb.Distance_Measured(t)
+    uwb.plot_results()
 
 
